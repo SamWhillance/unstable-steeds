853c54b39a4754a54986ce108bdb4fff82f2371a
Sam Whillance <samwhillance@gmail.com>
2016-08-09 21:17:39 +1000

    Changed light direction

---
+++ project/Assets/MakeTreeRandom.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeTreeRandom : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		// Make the tree scale slightly random
+		float newHeightScale = Random.Range(-0.5F, 0.6F);
+		float newWidthScale = Random.Range(-0.5F, 0.5F);
+		float distanceFromTrack;
+
+		transform.localScale += new Vector3(newWidthScale, newHeightScale, newWidthScale);
+	}
+}
